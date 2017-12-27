6d70c54fbffd4d0e16e3c94ce349c4114ea92fe8
630012164@qq.com <630012164@qq.com>
2017-12-27 15:49:00 +0800

    banner详情

--- app/Api/Mobile/Transformers/BannerinfoTransformer.php
+++ app/Api/Mobile/Transformers/BannerinfoTransformer.php
@@ -13,7 +13,8 @@ class BannerinfoTransformer extends TransformerAbstract
         $info = [
             'id' => $item->id,
             'name' => $item->name,
-            'img' => getImgAttribute($item->picture),
+            //'img' => getImgAttribute($item->picture),
+            'img' => 'http://mpic.tiankong.com/2a6/5a9/2a65a986200592edfab6c19809479984/640.jpg', // TODO
             'content' => $item->content,
         ];
         return $info;
